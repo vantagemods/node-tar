ec5042d2fb4a54a41c118c77fd5be91feeb0c772
vantagemods <vantagemods@users.noreply.github.com>
2017-06-17 06:36:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict'
 
 // high-level commands
-exports.c = exports.create = require('./lib/create.js')
-exports.r = exports.replace = require('./lib/replace.js')
-exports.t = exports.list = require('./lib/list.js')
-exports.u = exports.update = require('./lib/update.js')
-exports.x = exports.extract = require('./lib/extract.js')
+exports.c = exports.create = require('./lib/create')
+exports.r = exports.replace = require('./lib/replace')
+exports.t = exports.list = require('./lib/list')
+exports.u = exports.update = require('./lib/update')
+exports.x = exports.extract = require('./lib/extract')
 
 // classes
-exports.Pack = require('./lib/pack.js')
-exports.Unpack = require('./lib/unpack.js')
-exports.Parse = require('./lib/parse.js')
-exports.ReadEntry = require('./lib/read-entry.js')
-exports.WriteEntry = require('./lib/write-entry.js')
-exports.Header = require('./lib/header.js')
-exports.Pax = require('./lib/pax.js')
-exports.types = require('./lib/types.js')
+exports.Pack = require('./lib/pack')
+exports.Unpack = require('./lib/unpack')
+exports.Parse = require('./lib/parse')
+exports.ReadEntry = require('./lib/read-entry')
+exports.WriteEntry = require('./lib/write-entry')
+exports.Header = require('./lib/header')
+exports.Pax = require('./lib/pax')
+exports.types = require('./lib/types')
